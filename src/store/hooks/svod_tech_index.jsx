dae70f9d9eb33d7816fb7c8492b202139ff8b3a5
umer134 <twincki69@gmail.com>
2024-10-31 14:52:12 +0300

    sending to push

--- src/store/hooks/svod_tech_index.jsx
+++ src/store/hooks/svod_tech_index.jsx
@@ -1,11 +1,18 @@
 import { create } from "zustand";
 
 
+const createRowStore = (index) => {
+    return create((set) => ({
+
+    }))
+}
 
 
 
+const rowStores = [];
+for (let i = 0; i < 60; i++) {
+  rowStores.push(createRowStore(i));
+}
 
-
-
-
+export default rowStores;
 
